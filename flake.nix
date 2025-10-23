474f62c6f245d2f866547037a54e9d8ea72a5afd
Zoe Gagnon <zoe@zgagnon.com>
2025-10-22 22:15:44 -0400

    Fix regression in Nix Flake
    In PR #105, a regression was introduced into the nix flake
    which disabled consumers from access the package.
    
    This pr fixes that regression.
    
    Additionally adds a nix smoke test for a backstop against this in the future

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          packages.default = pkgs.callPackage { inherit pkgs self; };
+          packages.default = pkgs.callPackage ./default.nix { inherit pkgs self; };
 
           apps.default = {
             type = "app";
